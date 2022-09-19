196adc19b3addbc1a61904b138eef20b7e01978b
PATILGAYATRIG <110421842+PATILGAYATRIG@users.noreply.github.com>
2022-09-19 05:45:24 -0700

    Update Seventeen9.c

--- Seventeen9.c
+++ Seventeen9.c
@@ -1,9 +1,25 @@
 // Write a C program to sort a string array in ascending order
- #include<stdio.h>
- int main()
+#include<stdio.h>
+#include<string.h>
+int main()
  {
-     char str[100];
-     printf("Enter any string:");
-     fgets(str,100,stdin);
+     char str[ ]="gayatri";
+     int fre[150]={0};
+     int i=0;
+     while(str[i]!='\0')
+     {
+        fre[str[i]]++;
+        i++;
+     }
+     for(i=0;i<150;i++)
+     {
+         if(fre[i]!=0)
+         {
+                printf("%c",i);
+         }
+
+     }
+return 0;
+
 
  }
