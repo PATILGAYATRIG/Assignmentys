affb0bf532eb06701aef4533239d50b1c491d53f
Your Name <you@example.com>
2022-09-20 09:26:51 -0700

    update

---
+++ Fourth1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x=1;
+    while(x<6)
+    {
+        printf("MySirg\n");
+        x++;
+    }
+    return 0;
+}
---
+++ Fourth7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    while(x>0)
+    {
+        if(x%2==0)
+        {
+            printf("%d ",x);
+        }
+        x--;
+    }
+    return 0;
+
+
+}
